a545aff1f7ebafd669b875ba58df2e04668380a8
Ramandeep Chamba <ramanchamba98@gmail.com>
2024-09-16 17:24:53 +0530

    implemented a map using leaflet library

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,8 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from "leaflet";
 
 function Map() {
   const navigate = useNavigate();
@@ -9,23 +12,32 @@ function Map() {
   const lng = searchParams.get("lng");
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
-    >
-      <h1>Map</h1>
-      <h2>
-        Position: {lat}, {lng}
-      </h2>
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 23, lng: -11 });
-        }}
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Change Position
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker
+          position={[51.505, -0.09]}
+          icon={
+            new Icon({
+              iconUrl: markerIconPng,
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+            })
+          }
+        >
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
